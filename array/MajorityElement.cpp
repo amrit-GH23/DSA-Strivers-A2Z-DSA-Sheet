b2c6a4d2aa0e5a7d8871db9380c9739c0ca5bd10
amrit-GH23 <amritkumar2005btw@gmail.com>
2025-04-23 21:39:53 +0530

    Array : Majority Element

---
+++ array/MajorityElement.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+
+using namespace std;
+
+int majorityElement(vector<int>& nums) {
+    unordered_map<int,int>m;
+    int n=nums.size();
+    for(int i=0;i<n;i++){
+        m[nums[i]]++;
+    }
+    for(auto i=m.begin();i!=m.end();i++){
+        if(i->second>(n/2)){
+            return i->first;
+        }
+    } 
+    return 0;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    };
+   int ans= majorityElement(arr);
+   cout<<ans<<"\n";
+    return 0;
+}
