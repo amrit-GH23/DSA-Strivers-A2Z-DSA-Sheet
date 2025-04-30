e41159602049c251197cc778124dd409951ab018
amrit-GH23 <amritkumar2005btw@gmail.com>
2025-04-30 10:46:38 +0530

    added Array: Set Matrix Zeroes

---
+++ array/SetMatrixZeroes.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+void setZeroes(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    set<int> rows;
+    set<int> cols;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(matrix[i][j] == 0) {
+                rows.insert(i);
+                cols.insert(j);
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(rows.count(i) || cols.count(j)) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> arr(n, vector<int>(m));  
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    setZeroes(arr);  
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
