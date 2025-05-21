80f3112b5bfbdaaf0fa4add3491f3e8575c37318
amrit-GH23 <amritkumar2005btw@gmail.com>
2025-05-21 22:13:45 +0530

    Added binary search-Search Insert Position

--- Binary Search/SearchInsertPosition.cpp
+++ Binary Search/SearchInsertPosition.cpp
@@ -1,4 +1,4 @@
-//Binary Search
+//Search Insert Position
 //Time Complexity: O(n)
 //Space Complexity: O(1)
 
@@ -7,22 +7,19 @@
 
 using namespace std;
 
- int search(vector<int>& nums, int target) {
-         int n=nums.size();
-         int low=0;
-         int high=n-1;
-         while(low<=high){
-            int mid=(high+low)/2;
+int searchInsert(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
             if(nums[mid]==target)return mid;
-            else if(nums[mid]>target){
-                high=mid-1;
-            }
+            else if(nums[mid]>target)high=mid-1;
             else low=mid+1;
-         }
-         return -1;
+        }
+        return low;
     }
-
-
+    
 int main(){
     int n;
     cin>>n;
@@ -32,7 +29,7 @@ int main(){
     }
     int target;
     cin>>target;
-   int ans =search(arr,target);
+   int ans =searchInsert(arr,target);
    cout<<ans;
     return 0;
 }
